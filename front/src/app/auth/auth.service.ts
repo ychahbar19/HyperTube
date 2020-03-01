6f944fcc989b1eaa645257f3e1a4da466b1112dd
Adam <adam_ceciora@hotmail.com>
2020-03-01 18:26:05 +0100

    mainly visual changes + header when user is logged or not

--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
+import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) { }
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   // isAuthenticated(): boolean {
@@ -56,7 +62,8 @@ export class AuthService {
       .subscribe(response => {
         const token = response.token;
         this.token = token;
-      })
+        this.authStatusListener.next(true);
+      });
     // return this.http.post<any>('http://localhost:3000/signin', authData);
   }
 
--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
 
-  isLogged = false;
+  constructor(private authService: AuthService, private router: Router) { }
 
-  constructor(private router: Router) {
+  ngOnInit() {
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
   }
 
-  displaySignInButton(){
-    return (this.router.url !== '/signin' || this.isLogged);
+  displaySignInButton() {
+    return (this.router.url !== '/signin' && !this.userIsAuthenticated);
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
   }
 }
