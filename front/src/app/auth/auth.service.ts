c64b864e263a0912dacd2d02ba538128baf43238
Adam Ceciora <adamceciora@MacBook-Pro-de-Adam.local>
2020-03-19 11:19:03 +0100

    forgot password done

--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -12,8 +12,10 @@ export class AuthService {
   private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
   private creationStatusListener = new Subject<boolean>();
+  private resetStatusListener = new Subject<boolean>();
+  resetPasswordSuccessMessage: string;
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
     return this.token;
@@ -31,17 +33,19 @@ export class AuthService {
     return this.creationStatusListener.asObservable();
   }
 
-  createUser(
-    formData: {
-      avatar: File,
-      firstName: string,
-      lastName: string,
-      username: string,
-      email: string,
-      password: string,
-      confirmPassword: string
-    }
-  ) {
+  getResetStatusListener() {
+    return this.resetStatusListener.asObservable();
+  }
+
+  createUser(formData: {
+    avatar: File;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }) {
     const authData = new FormData();
     authData.append('photoUrl', formData.avatar);
     authData.append('firstName', formData.firstName);
@@ -50,46 +54,62 @@ export class AuthService {
     authData.append('email', formData.email);
     authData.append('password', formData.password);
     authData.append('confirmPassword', formData.confirmPassword);
-    this.http.post('http://localhost:3000/signup', authData)
-      .subscribe(response => {
+    this.http.post('http://localhost:3000/signup', authData).subscribe(
+      response => {
         this.authStatusListener.next(false);
         this.creationStatusListener.next(true);
         // garder l'id quelque part ou aller le rechercher au login pour le mettre en cookie
-      }, error => {
+      },
+      error => {
         this.authStatusListener.next(false);
-      });
+      }
+    );
   }
 
   activateAccount(id: string) {
-    return this.http.post('http://localhost:3000/api/auth/activateAccount', { id });
+    return this.http.post('http://localhost:3000/api/auth/activateAccount', {
+      id
+    });
   }
 
-  login(formData: { username: string, password: string }) {
-    const authData: AuthData = { username: formData.username, password: formData.password };
-    this.http.post<{ token: string, expiresIn: number }>('http://localhost:3000/signin', authData)
-      .subscribe(response => {
-        const token = response.token;
-        this.token = token;
-        if (token) {
-          const expiresInDuration = response.expiresIn;
-          this.setAuthTimer(expiresInDuration);
-          this.isAuthenticated = true;
-          this.authStatusListener.next(true);
-          const now = new Date();
-          const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
-          this.saveAuthData(token, expirationDate);
-          this.router.navigate(['/gallery']);
+  login(formData: { username: string; password: string }) {
+    const authData: AuthData = {
+      username: formData.username,
+      password: formData.password
+    };
+    this.http
+      .post<{ token: string; expiresIn: number }>(
+        'http://localhost:3000/signin',
+        authData
+      )
+      .subscribe(
+        response => {
+          const token = response.token;
+          this.token = token;
+          if (token) {
+            const expiresInDuration = response.expiresIn;
+            this.setAuthTimer(expiresInDuration);
+            this.isAuthenticated = true;
+            this.authStatusListener.next(true);
+            const now = new Date();
+            const expirationDate = new Date(
+              now.getTime() + expiresInDuration * 1000
+            );
+            this.saveAuthData(token, expirationDate);
+            this.router.navigate(['/gallery']);
+          }
+        },
+        error => {
+          this.authStatusListener.next(false);
         }
-      }, error => {
-        this.authStatusListener.next(false);
-      });
+      );
   }
 
   // Auto authenticate user if token exists and is still valid (duration wise)
   autoAuthUser() {
     const authInformation = this.getAuthData();
     if (!authInformation) {
-      return ;
+      return;
     }
     const now = new Date();
     // getTime() -> number of miliseconds since 1 jan 1970 and the date
@@ -131,7 +151,7 @@ export class AuthService {
     const token = localStorage.getItem('token');
     const expirationDate = localStorage.getItem('expiration');
     if (!token || !expirationDate) {
-      return ;
+      return;
     }
     return {
       token,
@@ -140,12 +160,42 @@ export class AuthService {
   }
 
   forgotPassword(formData: { username: string }) {
-    this.http.post('http://localhost:3000/api/auth/activateAccount', formData)
-      .subscribe(response => {
-        console.log(response);
-      }, error => {
-        console.log(error);
-      });
+    this.http
+      .post('http://localhost:3000/api/auth/forgotPassword', formData)
+      .subscribe(
+        response => {
+          // tslint:disable-next-line: max-line-length
+          this.resetPasswordSuccessMessage = 'We just sent you an email to the email address associated to this account. Check it and follow the steps';
+          this.authStatusListener.next(false);
+        },
+        error => {
+          // check if error then success if only one alert at once
+          console.log(error);
+        }
+      );
   }
 
+  resetPassword(
+    id: string,
+    hash: string,
+    formData: { password: string; confirmPassword: string }
+  ) {
+    const datas = {
+      id,
+      hash,
+      formData
+    };
+    this.http
+      .post('http://localhost:3000/api/auth/resetPassword', datas)
+      .subscribe(
+        response => {
+          this.resetPasswordSuccessMessage = 'Your password has been successfully changed! You may now log in with your new password';
+          this.authStatusListener.next(false);
+        },
+        error => {
+          // check if error then success if only one alert at once
+          console.log(error);
+        }
+      );
+  }
 }
--- front/src/app/auth/forgot-password/forgot-password.component.ts
+++ front/src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ErrorService } from 'src/app/error/error.service';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-forgot-password',
@@ -12,15 +16,61 @@ import { NgForm } from '@angular/forms';
 })
 export class ForgotPasswordComponent implements OnInit {
   @ViewChild('f', { static: false }) forgotForm: NgForm;
+  @ViewChild('f2', { static: false }) resetForm: NgForm;
+  private errorStatusSub: Subscription;
+  private authStatusSub: Subscription;
+  resetStatus = false;
+  errorMessage: string;
+  successMessage: string;
+  isLoading = false;
+  secondForm = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private errorService: ErrorService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(authStatus => {
+        this.successMessage = this.authService.resetPasswordSuccessMessage;
+        this.isLoading = false;
+      });
+    this.errorStatusSub = this.errorService.errorObs.subscribe(error => {
+      this.errorMessage = error;
+    });
+    if (
+      this.route.snapshot.queryParams.id &&
+      this.route.snapshot.queryParams.hash
+    ) {
+      this.secondForm = true;
+    }
+  }
 
   onForgotPwd() {
     if (this.forgotForm.invalid) {
       return;
     }
+    this.isLoading = true;
     this.authService.forgotPassword(this.forgotForm.value);
+  }
+
+  onResetPwd() {
+    if (this.resetForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.resetPassword(
+      this.route.snapshot.queryParams.id,
+      this.route.snapshot.queryParams.hash,
+      this.resetForm.value
+    );
+  }
+
+  samePwd(password1: string, password2: string): boolean {
+    return password1 === password2;
   }
 }
