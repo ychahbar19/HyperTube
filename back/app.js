0d7b65c4cee3fe9ff363efb99b6af7f0af44ca0f
Clio Brichaut <clio.brichaut@gmail.com>
2020-03-20 21:51:57 +0100

    login

--- back/app.js
+++ back/app.js
@@ -5,7 +5,7 @@
 \* -------------------------------------------------------------------------- */
 
 const express = require('express');
-const expressSession = require('express-session');//npm install express-session --save
+const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
