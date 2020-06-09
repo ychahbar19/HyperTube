374a098964570ebcf1375a58ebb6ce33aaebbcae
acecio <adam_ceciora@hotmail.com>
2020-06-09 18:34:29 +0200

    tslint errors

--- front/src/app/D-user/user.service.ts
+++ front/src/app/D-user/user.service.ts
@@ -2,15 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class UserService
-{
+export class UserService {
   constructor(private http: HttpClient) {}
 
   // Fetches the video info from the API (back) and returns it.
-  getUserInfo(user_id)
-  {
-    return new Promise((resolve, reject) =>
-    {
+  getUserInfo(user_id) {
+    return new Promise((resolve, reject) => {
       user_id = (user_id !== undefined) ? user_id : '';
 
       this.http.get<{}>('http://localhost:3000/api/user/profile/' + user_id)
