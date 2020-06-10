a7404487bc26c3f1204476a09f4b9428ab7f8b05
acecio <adam_ceciora@hotmail.com>
2020-06-10 17:41:33 +0200

    play btn on posters

--- front/src/app/C-auth/auth-interceptor.ts
+++ front/src/app/C-auth/auth-interceptor.ts
@@ -3,12 +3,10 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor
-{
+export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler)
-  {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = this.authService.getToken();
     const authRequest = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + authToken) });
     return next.handle(authRequest);
