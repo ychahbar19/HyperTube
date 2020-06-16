237bdf783c4660f5f2831ad4eea4e225c27a8427
acecio <adam_ceciora@hotmail.com>
2020-06-16 18:37:18 +0200

    tslint err

--- front/src/app/C-auth/auth.service.ts
+++ front/src/app/C-auth/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class AuthService {
   /* ------------------------------------------------------- *\
       Private and public variables.
@@ -63,18 +63,18 @@ export class AuthService {
   }) {
     // Translate the input into another format.
     const authData = new FormData();
-    authData.append("photoUrl", formData.avatar);
-    authData.append("firstName", formData.firstName);
-    authData.append("lastName", formData.lastName);
-    authData.append("username", formData.username);
-    authData.append("email", formData.email);
-    authData.append("password", formData.password);
-    authData.append("confirmPassword", formData.confirmPassword);
+    authData.append('photoUrl', formData.avatar);
+    authData.append('firstName', formData.firstName);
+    authData.append('lastName', formData.lastName);
+    authData.append('username', formData.username);
+    authData.append('email', formData.email);
+    authData.append('password', formData.password);
+    authData.append('confirmPassword', formData.confirmPassword);
 
     // Calls the API (back) for the signup process. If the signup is a success,
     // sets signupSuccessListener to TRUE. And in any case, once the process is done,
     // sets authServiceWorkingListener to FALSE.
-    this.http.post("http://localhost:3000/api/auth/signup", authData).subscribe(
+    this.http.post('http://localhost:3000/api/auth/signup', authData).subscribe(
       (response) => {
         this.signupSuccessListener.next(true);
         this.authServiceWorkingListener.next(false);
@@ -89,7 +89,7 @@ export class AuthService {
 
   activateAccount(userId: string) {
     // Calls the API's (back) activateAccount process
-    return this.http.post("http://localhost:3000/api/auth/activateAccount", {
+    return this.http.post('http://localhost:3000/api/auth/activateAccount', {
       id: userId,
     });
   }
@@ -125,7 +125,7 @@ export class AuthService {
     // calls applySuccessSignin(), and redirects to /search.
     this.http
       .post<{ token: string; expiresIn: number }>(
-        "http://localhost:3000/api/auth/signin",
+        'http://localhost:3000/api/auth/signin',
         formData
       )
       .subscribe(
@@ -133,7 +133,7 @@ export class AuthService {
           this.token = response.token;
           if (this.token) {
             this.connectUser(response);
-            this.router.navigate(["/search"]);
+            this.router.navigate(['/search']);
           }
         },
         (error) => {
@@ -154,7 +154,7 @@ export class AuthService {
     // And in any case, once the process is done,
     // sets authServiceWorkingListener to FALSE.
     this.http
-      .post("http://localhost:3000/api/auth/forgotPassword", formData)
+      .post('http://localhost:3000/api/auth/forgotPassword', formData)
       .subscribe(
         (response) => {
           this.resetPasswordSuccessMessage = 1;
@@ -184,7 +184,7 @@ export class AuthService {
     // If the reset is a success, sets the resetPasswordSuccessMessage.
     // And in any case, once the process is done, sets authServiceWorkingListener to FALSE.
     this.http
-      .post("http://localhost:3000/api/auth/resetPassword", datas)
+      .post('http://localhost:3000/api/auth/resetPassword', datas)
       .subscribe(
         (response) => {
           this.resetPasswordSuccessMessage = 2;
@@ -209,11 +209,11 @@ export class AuthService {
     email: string;
   }) {
     const updateData = new FormData();
-    updateData.append("photoUrl", formData.avatar);
-    updateData.append("firstName", formData.firstName);
-    updateData.append("lastName", formData.lastName);
-    updateData.append("username", formData.username);
-    updateData.append("email", formData.email);
+    updateData.append('photoUrl', formData.avatar);
+    updateData.append('firstName', formData.firstName);
+    updateData.append('lastName', formData.lastName);
+    updateData.append('username', formData.username);
+    updateData.append('email', formData.email);
 
     const authInformation = this.getAuthData();
     const now = new Date();
@@ -221,11 +221,11 @@ export class AuthService {
       (authInformation.expirationDate.getTime() - now.getTime()) /
       1000
     ).toString();
-    updateData.append("remainingTime", expiresInSeconds);
+    updateData.append('remainingTime', expiresInSeconds);
 
     return this.http
       .post<{ token: string; expiresIn: number }>(
-        "http://localhost:3000/api/user/editProfile",
+        'http://localhost:3000/api/user/editProfile',
         updateData
       )
       .subscribe(
@@ -252,7 +252,7 @@ export class AuthService {
     this.authServiceWorkingListener.next(false);
     clearTimeout(this.tokenTimer);
     this.clearAuthData();
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
   }
 
   /* ------------------------------------------------------------------ *\
@@ -261,14 +261,14 @@ export class AuthService {
 
   // Sets token and its expiration date in localStorage
   private saveAuthData(token: string, expirationDate: Date) {
-    localStorage.setItem("token", token);
-    localStorage.setItem("expiration", expirationDate.toISOString());
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
   }
 
   // Returns the token and its expiration date if both exist from localStorage
   private getAuthData() {
-    const token = localStorage.getItem("token");
-    const expirationDate = localStorage.getItem("expiration");
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
     if (!token || !expirationDate) {
       return;
     }
@@ -277,8 +277,8 @@ export class AuthService {
 
   // Deletes token and its expiration date from localStorage
   private clearAuthData() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("expiration");
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
   }
 
   // Sets timer after what user is disconnected
