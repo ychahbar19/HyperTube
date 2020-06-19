234692513156c22917532d671d0044767b4e0944
acecio <adam_ceciora@hotmail.com>
2020-06-19 11:48:47 +0200

    error handling if mail fails

--- back/controllers/MailController.js
+++ back/controllers/MailController.js
@@ -8,15 +8,12 @@ const nodemailer = require('nodemailer');
     2) Defines the base SENDMAIL function.
 \* -------------------------------------------------------------------------- */
 
-function sendMail(to, subject, html)
-{
-  const transporter = nodemailer.createTransport(
-  {
+function sendMail(to, subject, html) {
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: { user: 'a.ceciora@gmail.com', pass: 'gccpmyozhzyefcfi' }
   });
-  const mailOptions =
-  {
+  const mailOptions = {
     from: 'a.ceciora@gmail.com',
     to: to,
     subject: subject,
@@ -32,7 +29,7 @@ function sendMail(to, subject, html)
   transporter.sendMail(mailOptions, (error, info) =>
   {
     if (error)
-      console.log(error); //return res.status(500).json({ message: error });
+      throw error
     else
       console.log('Email send: ' + info.response);
   });
@@ -45,14 +42,18 @@ function sendMail(to, subject, html)
 // Signup confirmation email.
 exports.sendConfirmMail = (req, res) =>
 {
-  const to = req.body.email;
-  const subject = 'HyperTube Account Confirmation';
-  const html = `<h3>Hello ` + res.savedUser.firstName + ` !</h3>
-                <p>Please confirm your account by clicking on this 
-                  <a href="http://localhost:4200/signin?id=` + res.savedUser._id + `">link</a>.
-                </p>`;
-  sendMail(to, subject, html);
-  res.status(201).json({ message: 'User created', result: res.savedUser });
+  try {
+    const to = req.body.email;
+    const subject = 'HyperTube Account Confirmation';
+    const html = `<h3>Hello ` + res.savedUser.firstName + ` !</h3>
+                  <p>Please confirm your account by clicking on this 
+                    <a href="http://localhost:4200/signin?id=` + res.savedUser._id + `">link</a>.
+                  </p>`;
+    sendMail(to, subject, html);
+    res.status(201).json({ message: 'User created', result: res.savedUser });
+  } catch (error) {
+    return res.status(500).json({ message: 'Mail couldn not be sent' });
+  }
 };
 
 // Password reset email.
